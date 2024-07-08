e383849bbb2bb41d0ab1db8d07041247f52b7027
GusFSProg <gustavo.prog40@gmail.com>
2024-07-08 18:34:34 -0300

    novos testes

--- read-json-url.py
+++ read-json-url.py
@@ -5,12 +5,12 @@ st.set_page_config(layout="wide")
 
 
 def buscar_dados():
-    request = requests.get("https://blog-gus-api.vercel.app/get-all-posts")
+    request = requests.get("https://example.vercel.app/get-all")
     todos = json.loads(request.content)
     todos
     # print(todos)
     print(todos[1]['title'])
-    print(todos[1]['text'])
+    print(todos[1]['author'])
 
 if __name__ == '__main__':
   buscar_dados()
